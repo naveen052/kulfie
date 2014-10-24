af612e60c142fd78afba3abd3c678a64cbb52a33
Naveen Singh <naveensingh.org@gmail.com>
2014-10-24 13:37:32 +0530

    added new features

--- kulfie-new/app/scripts/main.js
+++ kulfie-new/app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  		$('.bxslider').bxSlider({
+  			auto: true,
+		mode: 'fade'
+  		});
+
+});
+
+$(document).ready(function(){
+	$("#tab1").click(function() {
+		$('#image1').removeClass('display-off');
+		$('#image2').addClass('display-off');
+		$('#image3').addClass('display-off');
+		$(this).addClass('active');
+		$('#tab2').removeClass('active');
+		$('#tab3').removeClass('active');		
+		})  
+
+	$("#tab2").click(function() {
+		$('#image1').addClass('display-off');
+		$('#image2').removeClass('display-off');
+		$('#image3').addClass('display-off');
+		$(this).addClass('active');
+		$('#tab1').removeClass('active');
+		$('#tab3').removeClass('active');			
+		})  
+
+	$("#tab3").click(function() {
+		$('#image1').addClass('display-off');
+		$('#image2').addClass('display-off');
+		$('#image3').removeClass('display-off');
+		$(this).addClass('active');
+		$('#tab1').removeClass('active');
+		$('#tab2').removeClass('active');			
+		})  
+});
